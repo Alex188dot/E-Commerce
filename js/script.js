6b4c1522fd07951dcef148a2bf06611c99bb74ab
alessio.leodori <alessio.leodori@gmail.com>
2023-08-24 11:24:21 +0200

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-fetch("https://striveschool-api.herokuapp.com/api/product/", {
-  headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU0N2JlMGRmZmI4YjAwMTQ0MTNiOTYiLCJpYXQiOjE2OTI2OTU1MjEsImV4cCI6MTY5MzkwNTEyMX0.73M6SmuYTdHsoW68z9q32Vhnb83McVN0sUMJY4HtaRQ",
-  },
-})
-  .then((response) => response.json())
-  .then((data) => {
-    // Handle the data here
-    console.log(data);
-  })
-  .catch((error) => {
-    // Handle any errors here
-    console.error(error);
-  });
-
 // Check if cartCounter exists in local storage
 if (!localStorage.getItem("cartItems")) {
   localStorage.setItem("cartItems", JSON.stringify([]));
@@ -74,7 +58,6 @@ window.addEventListener("load", () => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
 
     updateCartCounter();
-    console.log(cartItems);
   }
 
   // Update cart counter
